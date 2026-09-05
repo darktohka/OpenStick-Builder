f598e7d906140b8fbc66dcadd7f83f3abecd9976
Disyer <daniel@tohka.us>
2026-09-05 23:56:40 +0300

    Use native and libc6-dve

--- scripts/build_gt.sh
+++ scripts/build_gt.sh
@@ -5,7 +5,7 @@ SRCDIR=$(pwd)/src
 
 # install gt dependencies
 chroot ${CHROOT} /bin/sh \
-    -c " apt update; apt install libconfig-dev -y"
+    -c " apt update; apt install libconfig-dev libc6-dev -y"
 
 # build and install gt
 (
@@ -18,7 +18,6 @@ mkdir -p build
 cd build
 PKG_CONFIG_PATH=${CHROOT}/usr/lib/aarch64-linux-gnu/pkgconfig \
     ${SRCDIR}/libusbgx/configure \
-        --host aarch64-linux-gnu \
         --prefix=/usr \
         --with-sysroot=${CHROOT}
 )
@@ -28,8 +27,6 @@ make -C build CFLAGS="--sysroot=${CHROOT}" install
 rm -rf build/*
 PKG_CONFIG_PATH=${CHROOT}/usr/lib/pkgconfig:${CHROOT}/usr/lib/aarch64-linux-gnu/pkgconfig \
     cmake -DCMAKE_INSTALL_PREFIX=/usr \
-        -DCMAKE_CXX_COMPILER=aarch64-linux-gnu-g++ \
-        -DCMAKE_C_COMPILER=aarch64-linux-gnu-gcc \
         -DCMAKE_C_FLAGS=-I$(pwd)/dist/usr/include \
         -DCMAKE_C_FLAGS=-L$(pwd)/dist/usr/lib \
         -DCMAKE_FIND_ROOT_PATH_MODE_PROGRAM=NEVER \
