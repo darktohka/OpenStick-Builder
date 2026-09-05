037b3f9b519a5dc15527b6a3bf4e0bad12d45b52
Disyer <daniel@tohka.us>
2026-09-05 23:42:22 +0300

    Fix archive

--- scripts/debootstrap.sh
+++ scripts/debootstrap.sh
@@ -9,7 +9,7 @@ HOST_NAME=${HOST_NAME=openstick-debian}
 rm -rf ${CHROOT}
 
 debootstrap --foreign --arch arm64 \
-    --keyring keyrings/debian-archive-keyring.gpg ${RELEASE} ${CHROOT}
+    --keyring /usr/share/keyrings/debian-archive-keyring.gpg ${RELEASE} ${CHROOT}
 
 cp $(which qemu-aarch64-static) ${CHROOT}/usr/bin
 
